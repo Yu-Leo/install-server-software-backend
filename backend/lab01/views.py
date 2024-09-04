f057e11014b2e4ca4e3706940f7ff653a50f2c7b
Lev Yuvenskiy <levayu22@gmail.com>
2024-09-04 15:20:41 +0300

    Fix lists

--- backend/lab01/views.py
+++ backend/lab01/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 
+services = [
+    {'id': 1, 'title': 'Docker', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-docker.png',
+     'price': 100},
+    {'id': 2, 'title': 'NodeJS', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-node-js.png',
+     'price': 150},
+    {'id': 3, 'title': 'Python', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-python.png',
+     'price': 200},
+    {'id': 4, 'title': 'JS', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-javascript.png',
+     'price': 300},
+    {'id': 5, 'title': 'git', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-git.png', 'price': 400},
+]
+
+order = [
+    {'id': 1, 'title': 'Docker', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-docker.png',
+     'price': 100},
+    {'id': 2, 'title': 'NodeJS', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-node-js.png',
+     'price': 150},
+    {'id': 3, 'title': 'Python', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-python.png',
+     'price': 200},
+]
+
 
 def get_services(request):
     if request.method == "POST":
@@ -7,31 +28,20 @@ def get_services(request):
     else:
         input_text = ""
     print(input_text)
-    services = [
-        {'id': 1, 'title': 'Docker', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-docker.png', 'price': 100},
-        {'id': 2, 'title': 'NodeJS', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-node-js.png', 'price': 150},
-        {'id': 3, 'title': 'Python', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-python.png', 'price': 200},
-        {'id': 4, 'title': 'JS', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-javascript.png', 'price': 300},
-        {'id': 5, 'title': 'git', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-git.png', 'price': 400},
-    ]
 
     return render(request, 'services.html',
                   {'data': {
-                      'services': [i for i in services if i["title"].startswith(input_text)]
-                  }})
+                      'services': [i for i in services if i["title"].startswith(input_text)],
+                      'count': len(order)
+                  },})
 
 
 def get_service(request, id):
-    services = {
-        1: {'title': 'Docker', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-docker.png', 'price': 100, 'size': '500Mb'},
-        2: {'title': 'NodeJS', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-node-js.png', 'price': 150, 'size': '100Mb'},
-        3: {'title': 'Python', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-python.png', 'price': 200, 'size': '1Gb'},
-        4: {'title': 'JS', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-javascript.png', 'price': 300, 'size': '800Mb'},
-        5: {'title': 'git', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-git.png', 'price': 400, 'size': '30Mb'},
-    }
-
-    return render(request, 'service.html',
-                  {'data': services[id]})
+    for i in services:
+        if i['id'] == id:
+            return render(request, 'service.html',
+                          {'data': i})
+    render(request, 'service.html')
 
 
 def get_order(request, id):
@@ -40,9 +50,5 @@ def get_order(request, id):
 
     return render(request, 'order.html',
                   {'data': {
-                      'services': [
-                          {'id': 1, 'title': 'Docker', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-docker.png', 'price': 100},
-                          {'id': 2, 'title': 'NodeJS', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-node-js.png', 'price': 150},
-                          {'id': 3, 'title': 'Python', 'logo_file_name': 'http://172.19.0.3:9000/server-soft-logos/logo-python.png', 'price': 200},
-                      ]
+                      'services': order,
                   }})
