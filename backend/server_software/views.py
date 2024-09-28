c17371f068910c3ed531034cba59818b81ab04df
Lev Yuvenskiy <yuvenskiylev@mail.ru>
2024-09-28 18:18:57 +0300

    Implement install_software_requests/<int:pk>/put

--- backend/server_software/views.py
+++ backend/server_software/views.py
@@ -166,7 +166,21 @@ def PutInstallSoftwareRequest(request, pk):
     """
     Изменение заявки на установку ПО
     """
-    return Response("Not implemented", status=501)  # TODO
+    install_software_request = InstallSoftwareRequest.objects.filter(id=pk,
+                                                                     status=InstallSoftwareRequest.RequestStatus.DRAFT).first()
+    if install_software_request is None:
+        return Response("InstallSoftwareRequest not found", status=status.HTTP_404_NOT_FOUND)
+
+    serializer = InstallSoftwareRequestSerializer(install_software_request,
+                                                  data=request.data,
+                                                  partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        install_software_request = InstallSoftwareRequest.objects.get(id=pk)
+        serializer = InstallSoftwareRequestSerializer(install_software_request)
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['PUT'])
@@ -174,6 +188,7 @@ def FormInstallSoftwareRequest(request, pk):
     """
     Формирование заявки на установку ПО
     """
+
     return Response("Not implemented", status=501)  # TODO
 
 
